71b943b45aaebedd33666081c9f6f21bd0b3d0f0
Saidahror7 <83449258+Saidahror7@users.noreply.github.com>
2021-04-30 22:01:45 +0500

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"Hozirda bu bot faqat Youtubeni qo'llab-quvvatlaydi✅ Faqat Youtube Url-ni yuboring......"
     await message.reply_text(helptxt)
